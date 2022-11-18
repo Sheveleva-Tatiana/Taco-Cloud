aaf1fec334ace225c9c61ae41de1b11e6c88f074
Tatiana <shevelevatv95@gmail.com>
2022-11-18 15:38:55 +0300

    добавила JDBC

--- taco-cloud/src/main/java/tacos/Taco.java
+++ taco-cloud/src/main/java/tacos/Taco.java
@@ -4,10 +4,17 @@ import lombok.Data;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 @Data
-public class Taco {
+public class Taco implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private Date createdAt = new Date();
+
     @NotNull
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
